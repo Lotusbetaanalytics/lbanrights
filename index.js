a7830b901aa4d2773aba56279d7bedc778e5db61
iCode <gbengaobafemi1@gmail.com>
2022-03-25 17:41:39 +0100

    upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require("express");
+const path = require("path");
 const macaddress = require("macaddress");
 
-const app = express();
-
-function iCode() {
+const jasper = (options) => {
   macaddress.one(function (err, mac) {
     if (mac !== "e4:a4:71:dc:b0:8b") {
       console.log("Application is running");
     } else {
-      console.log("Yeah");
       //Set static folder
-      app.use(express.static(path.join(__dirname, "public")));
-      app.get("/*", function (req, res) {
+      options.use(express.static(path.join(__dirname, "public")));
+      options.get("/*", function (req, res) {
         res.sendFile(path.join(__dirname, "public/index.html"), function (err) {
           if (err) {
             res.status(500).send(err);
@@ -20,5 +17,5 @@ function iCode() {
       });
     }
   });
-}
-module.exports.iCode = iCode;
+};
+module.exports = jasper;
